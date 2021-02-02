8867c58acaed2f82620fede64bd1e3f6a77540a2
ripleyhuang <ripleyhuang@tencent.com>
2021-02-02 22:06:12 +0800

    battry demo

---
+++ batterylevel/ios/Runner/AppDelegate.swift
@@ -0,0 +1,44 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    GeneratedPluginRegistrant.register(with: self);
+
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController;
+    let batteryChannel = FlutterMethodChannel.init(name: "samples.flutter.io/battery",
+                                                   binaryMessenger: controller as! FlutterBinaryMessenger);
+    batteryChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        
+        batteryChannel.setMethodCallHandler({
+          (call: FlutterMethodCall, result: FlutterResult) -> Void in
+          if ("getBatteryLevel" == call.method) {
+            receiveBatteryLevel(result: result);
+          } else {
+            result(FlutterMethodNotImplemented);
+          }
+        });
+        
+        
+      // Handle battery messages.
+    });
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions);
+  }
+    
+}
+private func receiveBatteryLevel(result: FlutterResult) {
+  let device = UIDevice.current;
+  device.isBatteryMonitoringEnabled = true;
+    if (device.batteryState == UIDevice.BatteryState.unknown) {
+    result(FlutterError.init(code: "UNAVAILABLE",
+                             message: "电池信息不可用",
+                             details: nil));
+  } else {
+    result(Int(device.batteryLevel * 100));
+  }
+}
