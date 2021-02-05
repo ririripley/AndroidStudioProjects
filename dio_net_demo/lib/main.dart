89e59e127cce5eb1e207638a1d4432ae5ac759a3
ripleyhuang <ripleyhuang@tencent.com>
2021-02-05 14:43:53 +0800

    添加网络

--- dio_net_demo/lib/main.dart
+++ dio_net_demo/lib/main.dart
@@ -1,31 +1,140 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'dart:io';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+
+
+class CacheInterceptor extends Interceptor {
+  CacheInterceptor();
+
+  var _cache = new Map<Uri, Response>();
+
+  @override
+  Future onRequest(RequestOptions options) async {
+    options.baseUrl = "https://www.google.com.hk/";
+    return options;
+  }
+
+  @override
+  Future onResponse(Response response) async{
+    _cache[response.request.uri] = response;
+  }
+
+  @override
+  Future onError(DioError e) async{
+    print('onError: $e');
+    if (e.type == DioErrorType.CONNECT_TIMEOUT || e.type == DioErrorType.DEFAULT) {
+      var cachedResponse = _cache[e.request.uri];
+      if (cachedResponse != null) {
+        return cachedResponse;
+      }
+    }
+    return e;
+  }
+}
+
+
+// 自己定义一个interceptor
+class AppInterceptors extends Interceptor {
+
+  @override
+  Future onRequest(RequestOptions options) async {
+    if (options.headers.containsKey("requiresToken")) {
+      //remove the auxiliary header
+      print("I am here !");
+      options.headers.remove("requiresToken");
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      var header = prefs.get("Header");
+
+      options.headers.addAll({"Header": "$header${DateTime.now()}"});
+      options.baseUrl = "https://www.google.com.hk";
+      return options;
+    }
+  }
+
+  @override
+  Future onError(DioError err) async {
+    if (err.message.contains("ERROR_001")) {
+      // this will push a new route and remove all the routes that were present
+      print("error");
+    }
+    return err;
+  }
+
+
+  @override
+  Future onResponse(Response response) async {
+    if (response.headers.value("counter") == null) {
+      //if the header is present, then compare it with the Shared Prefs key
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      var verifyToken = prefs.get("counter");
+
+      // if the value is the same as the header, continue with the request
+      // if (response.headers.value("counter") == verifyToken) {
+      //   return response;
+      // }
+      return response;
+    }
+
+    return DioError(request: response.request);
+  }
+}
+
+
+
+
 
 void main() async {
-  var dio = Dio(BaseOptions(
-    baseUrl: "http://httpbin.org/",
-    connectTimeout: 5000,
-    receiveTimeout: 100000,
-    // 5s
-    headers: {
-      HttpHeaders.userAgentHeader: "dio",
-      "api": "1.0.0",
-    },
-    contentType: Headers.jsonContentType,
-    // Transform the response data to a String encoded with UTF8.
-    // The default value is [ResponseType.JSON].
-    responseType: ResponseType.plain, //string 格式
-  ));
 
-  Response response;
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SharedPreferences prefs = await SharedPreferences.getInstance();
+  // prefs.setInt("counter", 1);
 
-  // response = await dio.get("/get");
-  //
-  // print("Response type: String:");
-  // print(response.data);
-  //
-  //
+ 
+
+  /// Extending Interceptor Class
+  // Response response;
+
+//   // (prefs.getInt('counter')
+//   dio.interceptors.add(InterceptorsWrapper(
+// /// 拦截器只会将当前的Options覆盖
+//       onRequest: (RequestOptions options) {
+//     // print(options.connectTimeout);
+//     // print(options.baseUrl);
+//     // print(options.receiveTimeout);
+//     // print(options.headers["api"]);
+//     // print(options.headers["user-agent"]);
+//     // print(options.contentType);
+//     // print(options.responseType);
+//     options.headers.addAll({"api": "1.0.0"});
+//     options.baseUrl = "";
+//     return options; //continue
+//   }, onResponse: (Response response) {
+//     // Do something with response data
+//     // print("Response type: String");
+//     // print(response.data);
+//     return response; // continue
+//   }, onError: (DioError e) {
+//     // Do something with response error
+//     print(e);
+//     return e; //continue
+//   }));
+
+  
+  // print("Now I am to send requests.");
+  // response = await dio.get("/");
+  // print(dio.options.responseType);
+  // print(dio.options.headers["api"]);
+  // print(dio.options.headers["user-agent"]);
+  // // print("Requests finish    + ." + response.data);
+  // // print("Response type: String:");
+  // print(response);
+  // print(dio.options.baseUrl);
+
+
+
   // Response<Map> responseMap = await dio.get(
   //   "/get",
   //   // Transform response data to Json Map
@@ -33,9 +142,9 @@ void main() async {
   // );
   // print("Response type: json:");
   // print(responseMap.data);
-  //
-  //
-  //
+
+
+
   //
   // response = await dio.post(
   //   "/post",
@@ -49,25 +158,28 @@ void main() async {
   //   ),
   // );
   // print(response.data);
-
+  //
   // response = await dio.request(
   //   "/",
   //   options: RequestOptions(baseUrl: "https://baidu.com"),
   // );
+  
+
+
   // print(response.data);
 
 
 
-  response = await dio.get("https://www.google.com");
-  print("data: ");
-  print(response.data);
-  print("headers: ");
-  print(response.headers);
-  print("request: ");
-  print(response.request);
-  print("status code: ");
-  print(response.statusCode);
-
+  // response = await dio.get("https://www.google.com");
+  // print("data: ");
+  // print(response.data);
+  // print("headers: ");
+  // print(response.headers);
+  // print("request: ");
+  // print(response.request);
+  // print("status code: ");
+  // print(response.statusCode);
+  runApp(MyApp());
 }
 
 
@@ -78,6 +190,21 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  var dio = Dio(BaseOptions(
+    baseUrl: "https://www.baidu.com/",
+    connectTimeout: 5000,
+    receiveTimeout: 100000,
+    // 5s
+    headers: {
+      HttpHeaders.userAgentHeader: "dio",
+      "requiresToken": true,
+    },
+    contentType: Headers.jsonContentType,
+    // Transform the response data to a String encoded with UTF8.
+    // The default value is [ResponseType.JSON].
+    responseType: ResponseType.plain, //string 格式
+  ));
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -98,13 +225,15 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page', dio: dio),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+
+  var dio;
+  MyHomePage({Key key, this.title, this.dio}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -116,61 +245,70 @@ class MyHomePage extends StatefulWidget {
   // always marked "final".
 
   final String title;
+  
+  get dioObj {
+    dio.interceptors.add(new AppInterceptors());
+    return dio;
+  }
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  int _counter = 0;
+  String response = "None";
+
+  void _incrementCounter(){
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  Widget _getInfoMessage(String msg) {
+    return Center(
+      child: Text(msg),
+    );
+  }
+
+  Future<Response> _getNetInfo() async {
+    Response response = await widget.dioObj.get("/");
+    return response;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    print("Build only at the beginning");
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
+            ),
+            FutureBuilder(
+              future:   _getNetInfo(),
+              builder: (buildContext, snapshot) {
+                print("setState invokes me !");
+                if (snapshot.hasError) {
+                  return _getInfoMessage(snapshot.error.toString());
+                }
+
+                if (!snapshot.hasData) {
+                  return Text("None");
+                }
+                Response res = snapshot.data; //_getListData()的返回值
+                return Text(res.data);
+              },
             ),
             Text(
               '$_counter',
